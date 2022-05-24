f514b0447bd47d42456c9d489a23bbf72760a3d4
rcastrof <85361103+rcastrof@users.noreply.github.com>
2022-05-23 21:21:38 -0400

    Add files via upload

---
+++ Prueba/contacto.php
@@ -0,0 +1,37 @@
+<?php
+    include 'template/header.php';
+?>
+
+
+    <!--contenido Servicios-->
+    <main class="contenedor sombra">
+        <div>
+
+
+            <h2>Contacto</h2>
+            <form action="#" method="POST">
+                <div>
+
+                    <label>Nombre</label>
+                    <input type="text" name="nombre" placeholder="Nombre" required>
+
+                    <label>Correo</label>
+                    <input type="mail" name="email" placeholder="email" required>
+
+                    <label>Mensaje</label>
+                    <textarea name="nombre" id="Mensaje" cols="30" rows="10"></textarea>
+
+                </div>
+                <div>
+                    <input type="submit" value="Enviar">
+                </div>
+            </form>
+
+        </div>
+    </main>
+    <!--contenido Contacto-->
+
+
+    <?php
+    include 'template/footer.php';
+?>
---
+++ Prueba/template/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <title>Onda</title>
+
+<!--<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+
+<body>
+
+            <!--Menu principal -->
+            <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">        
+        <a class="nav-link" href="#">Crear cuenta</a>
+        <a class="nav-link" href="#">Iniciar sesion</a>
+        <a class="nav-link disabled"></a>
+        <a class="nav-link disabled"></a>
+        <a class="nav-link disabled"></a>
+        <a class="nav-link disabled"></a>
+        <a class="nav-link" href="#">Diseñadores</a>
+        <a class="nav-link" href="#">Crea tu propio diseño</a>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+    <!--contenido Personal-->
+    <section class="hero">
+    <div class="contenido-hero">
+        <h2 class="titulo">Onda </h2>
+        <div class="ubicacion">
+                <p>
+                    <select class="form-select" aria-label="Default select example" style="width:800px" >
+                        <option selected>Selecciona tu region</option>
+                        <option value="1">Santiago</option>
+                        <option value="2">Temuco</option>                        
+                      </select>
+                      
+                </p>                
+            </div>      
+    </div>
+    </section>
