4c9c459954304df5818c60fec3f7136aa03f2dc5
rcastrof <85361103+rcastrof@users.noreply.github.com>
2022-05-24 20:50:49 -0400

    Add files via upload

---
+++ Prueba/index.php
@@ -0,0 +1,44 @@
+<?php
+    include 'template/header.php';
+?>
+
+ 
+    <main class="contenedor sombra"> <!-- Contenedor sombra es el fondo blanco-->
+    
+    <div class="carrusel">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="2000">
+      <img src="/img/unknown.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="/img/unknownr.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item"data-bs-interval="2000">
+      <img src="/img/unknownr.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+        </div>
+        <div class="contenedor">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, voluptas.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cum corrupti quia nulla consectetur cumque iusto, eos id in quas ullam esse ipsum beatae aspernatur at natus doloribus velit. Ex, alias? Libero aut quod odio nisi natus earum. Non adipisci nulla eligendi commodi ab esse! Maiores, inventore aut deleniti eligendi necessitatibus voluptatibus, placeat cum officia quae quo, ut fuga reprehenderit ab doloremque sint numquam obcaecati? Sint minima praesentium at eaque officiis ea dolorem esse, quae voluptatum perspiciatis, animi labore. Perferendis sequi iste nostrum, quis quaerat a cum quos minus et molestias veritatis nemo similique sit voluptates veniam nulla id tenetur!
+            </p>
+        </div>
+</main>
+    <!--contenido Contacto-->
+
+<?php
+    include 'template/footer.php';
+?>
